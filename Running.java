b0ff3b844aa79eb201db1104626bb91a0554eecc
AlexJPorter <42015930+AlexJPorter@users.noreply.github.com>
2018-08-01 21:45:48 -0400

    Add files via upload

---
+++ Running.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Running here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Running
+{
+public static void main (String [] args) {
+    PigGame testing = new PigGame();
+    testing.autoGame(); 
+    testing.restart();
+    
+}
+}
